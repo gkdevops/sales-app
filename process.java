b01a8d785a8bc147adc0500a53fe465fd2b3b4f7
Goutham Kumar <47983326+gkdevops@users.noreply.github.com>
2023-02-25 07:48:43 +0530

    Added new print statement

--- process.java
+++ process.java
@@ -5,6 +5,8 @@ public class HelloWorld {
 
         // Prints "Hello, World" in the terminal window.
         System.out.println("Hello, World");
+        
+        System.out.println("Hello, Sales Team");
     }
 
 }
